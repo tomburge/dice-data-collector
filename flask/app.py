0bf3da92ba00775f68d30ca763ab8a2615b85c11
Tom Burge <tom@vcix.ninja>
2019-06-14 13:18:26 -0500

    refactored container
    container now uses flask, celery and redis to run tasks in background

--- flask/app.py
+++ flask/app.py
@@ -9,24 +9,32 @@ from flask_assets import Bundle, Environment
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, InputRequired
-from celery import Celery
+from celery import Celery, task
 
 # my imports
 from vrops import pull_data_from_vrops, get_list_of_json_files
 from push_to_dice import dice_transmit
+import config
 
+# ---------------------------------------------------------------------------------
+
+# initialize flask app
 application = Flask(__name__)
 application.config['SECRET_KEY'] = 'supersecretkey'
 
 # celery config
-application.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
-application.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+application.config.from_object('config')
 
 # celery initialization
-celery = Celery(application.name, broker=application.config['CELERY_BROKER_URL'])
+celery = Celery(
+    application.import_name,
+    backend=application.config['CELERY_RESULT_BACKEND'],
+    broker=application.config['CELERY_BROKER_URL']
+)
 celery.conf.update(application.config)
 
 
+# environment config
 env = Environment(application)
 js = Bundle('js/clr-icons.min.js', 'js/clr-icons-api.js','js/clr-icons-element.js', 'js/custom-elements.min.js')
 env.register('js_all', js)
@@ -46,6 +54,20 @@ class TransmitForm(FlaskForm):
     json_file = StringField('DICE JSON File ', validators=[InputRequired(), DataRequired()])
     submit = SubmitField('Send To DICE')
 
+@celery.task(name='call.vrops.connect')
+def call_vrops_connect(vropshost, vropsuser, vropspass, customer_id):
+    vhost = vropshost
+    vuser = vropsuser
+    vpass = vropspass
+    cust_id = customer_id
+    pull_data_from_vrops(vhost, vuser, vpass, cust_id)
+
+@celery.task(name='call.test.call')
+def test_call():
+    test_call = 'test_call was called\n'
+    with open('test_call.log', 'a') as j:
+        j.write(test_call)
+
 @application.route("/", methods=['GET'])
 def index():
     form = LoginForm()
@@ -58,10 +80,7 @@ def vrops_connect():
         vropsuser = request.form.get('vropsuser')
         vropspass = request.form.get('vropspass')
         customer_id = request.form.get('customer_id')
-        response = pull_data_from_vrops(vropshost, vropsuser, vropspass, customer_id)
-        dice_file = 'dice_vrops_output_' + str(datetime.date.today()) + '.json'
-        with open('static/json/' + dice_file, 'w') as j:
-            json.dump(response, j, indent=4)
+        celery.send_task('call.vrops.connect', args=(vropshost, vropsuser, vropspass, customer_id))
         return redirect('get-json')
 
 @application.route("/example")
@@ -90,4 +109,4 @@ def about():
     return render_template('about.html')
 
 if __name__ == "__main__":
-    application.run(host="0.0.0.0", port=5000)
+    application.run(host="0.0.0.0")
