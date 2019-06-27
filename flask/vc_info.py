260f0f01ba5a5f08bb6cdce6477475ec1435ed34
Tom Burge <tburge@vmware.com>
2019-06-27 13:17:05 -0500

    updated vm_ds

--- flask/vc_info.py
+++ flask/vc_info.py
@@ -54,6 +54,12 @@ def get_vm_info(vm, depth=1, max_depth=20):
             else:
                 switch.append('error')
 
+    vm_ds = ''
+    if config.datastoreUrl:
+        vm_ds = config.datastoreUrl[0].name
+    else:
+        vm_ds = 'error'
+
     instanceUuid = summary.config.instanceUuid if summary.config.instanceUuid is not None else 'error'
     datacenter = parent.parent.name if parent.parent.name is not None else 'error'
     cluster = summary.runtime.host.parent.name if summary.runtime.host.parent.name is not None else 'error'
@@ -80,7 +86,7 @@ def get_vm_info(vm, depth=1, max_depth=20):
     tools_run_status = summary.guest.toolsRunningStatus if summary.guest.toolsRunningStatus is not None else 'error'
     tools_version = config.tools.toolsVersion if config.tools.toolsVersion is not None else 'error'
     vhw_version = config.version if config.version is not None else 'error'
-    vm_ds = config.datastoreUrl[0].name if config.datastoreUrl is not None else 'error'
+    # vm_ds = config.datastoreUrl[0].name if config.datastoreUrl is not None else 'error'
     portgroup = network[0].name if network[0].name is not None else 'error'
     res_pool = resource_pool.name if resource_pool is not None else 'error'
     power_state = summary.runtime.powerState if summary.runtime.powerState is not None else 'error'
