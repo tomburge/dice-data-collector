03523a88ef79b0289e02e39b753b0170681f13e9
Tom Burge <tom@vcix.ninja>
2019-07-27 10:22:10 -0500

    updated update script

--- flask/update.sh
+++ flask/update.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 # this script updates the appliance from github
 cd
+rm ran_customization
 cd dice-data-collector
 docker-compose down
 cd ..
 rm -rf dice-data-collector
-docker system prune -a
+docker system prune -a -f
 cd
 git clone https://github.com/tomburge/dice-data-collector.git
 cd dice-data-collector
